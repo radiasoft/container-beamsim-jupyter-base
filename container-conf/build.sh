0b01be558a6f1590bac4b2f9e6a162ca099f9c05
robnagler <github@q33.us>
2017-02-28 12:13:39 +0000

    refactored for consistent naming

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,5 +1,72 @@
 #!/bin/bash
-build_vars() {
+
+beamsim_jupyter_build_jupyter() {
+    # update python-build
+    pyenv update || true
+
+    local pyver=3.5.2
+
+    pyenv install "$pyver"
+    pyenv virtualenv "$pyver" "$jupyter_venv"
+    pyenv activate "$jupyter_venv"
+
+    pip install --upgrade pip
+    pip install --upgrade setuptools==32.1.3
+    # Since the Terminado Settings has not been merged into any release, we are have your
+    # own fork with the changes applied to the latest release.
+    pip install --upgrade --no-deps git+https://github.com/radiasoft/notebook@terminado_settings#egg=notebook
+    pip install jupyter jupyterlab jupyterhub
+
+    jupyter serverextension enable --py jupyterlab --sys-prefix
+}
+
+beamsim_jupyter_default_py2_kernel() {
+    local where=( $(python -m ipykernel install --display-name 'Python 2' --name "$(pyenv global)" --user) )
+    . ~/.pyenv/pyenv.d/exec/*synergia*.bash
+    beamsim_jupyter_ipy_kernel_env "${where[-1]}"
+}
+
+beamsim_jupyter_ipy_kernel_env() {
+    local where="$1"
+    perl -pi -e 'sub _e {join(qq{,\n},
+            map(qq{  "$_": "$ENV{$_}"},
+                qw(SYNERGIA2DIR LD_LIBRARY_PATH PYTHONPATH)))};
+        s/^\{/{\n "env": {\n@{[_e()]}\n },/' "$where/kernel.json"
+}
+
+beamsim_jupyter_rsbeams_style() {
+    cd /tmp
+    git clone https://github.com/radiasoft/rsbeams
+    for src in rsbeams/rsbeams/matplotlib/stylelib/*; do
+        dst=~/.config/matplotlib/$(basename "$src")
+        cp -a "$src" "$dst"
+    done
+    rm -rf rsbeams
+}
+
+beamsim_jupyter_synergia_pre3() {
+    #TODO(robnagler) do this in radiasoft/beamsim/codes/synergia.sh
+    #      need something to avoid hardwiring openmpi lib... PYTHONPATH is
+    #      also not right, but we control everything here so probably ok.
+    pip install -U 'git+git://github.com/radiasoft/rsbeams.git@master'
+    pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
+    local venv=synergia-pre3
+    pyenv virtualenv 2.7.10 "$venv"
+    pyenv activate "$venv"
+    # pykern brings in a lot of requirements to simplify build times
+    pip install pykern
+    build_curl radia.run | codes_synergia_branch=devel-pre3 bash -s master code synergia
+    pip install -U 'git+git://github.com/radiasoft/rsbeams.git@master'
+    pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
+    # http://ipython.readthedocs.io/en/stable/install/kernel_install.html
+    # http://www.alfredo.motta.name/create-isolated-jupyter-ipython-kernels-with-pyenv-and-virtualenv/
+    local where=( $(python -m ipykernel install --display-name 'Python 2 synergia-pre3' --name "$venv" --user) )
+    . ~/.pyenv/pyenv.d/exec/*synergia*.bash
+    beamsim_jupyter_ipy_kernel_env "${where[-1]}"
+    # Test with: ipython notebook --no-browser --ip='*'
+}
+
+beamsim_jupyter_vars() {
     build_image_base=radiasoft/beamsim
     boot_dir=$build_run_user_home/.radia-run
     tini_file=$boot_dir/tini
@@ -20,67 +87,9 @@ build_as_root() {
     yum-config-manager --disable 'rpmfusion*' > /dev/null
 }
 
-build_default_py2_kernel() {
-    local where=( $(python -m ipykernel install --display-name 'Python 2' --name "$(pyenv global)" --user) )
-    . ~/.pyenv/pyenv.d/exec/*synergia*.bash
-    update_ipy_kernel_env "${where[-1]}"
-}
-
-build_synergia_pre3() {
-    #TODO(robnagler) do this in radiasoft/beamsim/codes/synergia.sh
-    #      need something to avoid hardwiring openmpi lib... PYTHONPATH is
-    #      also not right, but we control everything here so probably ok.
-    pip install -U 'git+git://github.com/radiasoft/rsbeams.git@master'
-    pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
-    local venv=synergia-pre3
-    pyenv virtualenv 2.7.10 "$venv"
-    pyenv activate "$venv"
-    # pykern brings in a lot of requirements to simplify build times
-    pip install pykern
-    build_curl radia.run | codes_synergia_branch=devel-pre3 bash -s master code synergia
-    pip install -U 'git+git://github.com/radiasoft/rsbeams.git@master'
-    pip install -U 'git+git://github.com/radiasoft/rssynergia.git@master'
-    # http://ipython.readthedocs.io/en/stable/install/kernel_install.html
-    # http://www.alfredo.motta.name/create-isolated-jupyter-ipython-kernels-with-pyenv-and-virtualenv/
-    local where=( $(python -m ipykernel install --display-name 'Python 2 synergia-pre3' --name "$venv" --user) )
-    . ~/.pyenv/pyenv.d/exec/*synergia*.bash
-    update_ipy_kernel_env "${where[-1]}"
-    # Test with: ipython notebook --no-browser --ip='*'
-}
-
-build_rsbeams_style() {
-    cd /tmp
-    git clone https://github.com/radiasoft/rsbeams
-    for src in rsbeams/rsbeams/matplotlib/stylelib/*; do
-        dst=~/.config/matplotlib/$(basename "$src")
-        cp -a "$src" "$dst"
-    done
-    rm -rf rsbeams
-}
-
-build_jupyter() {
-    # update python-build
-    pyenv update || true
-
-    local pyver=3.5.2
-
-    pyenv install "$pyver"
-    pyenv virtualenv "$pyver" "$jupyter_venv"
-    pyenv activate "$jupyter_venv"
-
-    pip install --upgrade pip
-    pip install --upgrade setuptools==32.1.3
-    # Since the Terminado Settings has not been merged into any release, we are have your
-    # own fork with the changes applied to the latest release.
-    pip install --upgrade --no-deps git+https://github.com/radiasoft/notebook@terminado_settings#egg=notebook
-    pip install jupyter jupyterlab jupyterhub
-
-    jupyter serverextension enable --py jupyterlab --sys-prefix
-}
-
 build_as_run_user() {
     cd "$build_guest_conf"
-    build_vars
+    beamsim_jupyter_vars
     local notebook_dir_base=jupyter
     export notebook_dir=$build_run_user_home/$notebook_dir_base
     export boot_dir
@@ -88,7 +97,7 @@ build_as_run_user() {
     export notebook_template_dir="$boot_dir/$notebook_dir_base"
     export jupyter_venv=jupyter
 
-    (build_jupyter)
+    (beamsim_jupyter_build_jupyter)
 
     # POSIT: notebook_dir in salt-conf/srv/pillar/jupyterhub/base.yml
     mkdir -p ~/.jupyter "$notebook_dir" "$notebook_template_dir"
@@ -104,17 +113,9 @@ build_as_run_user() {
     build_replace_vars post_bivio_bashrc ~/.post_bivio_bashrc
     . ~/.bashrc
 
-    (build_default_py2_kernel)
-    (build_rsbeams_style)
-    (build_synergia_pre3)
+    (beamsim_jupyter_default_py2_kernel)
+    (beamsim_jupyter_rsbeams_style)
+    (beamsim_jupyter_synergia_pre3)
 }
 
-update_ipy_kernel_env() {
-    local where="$1"
-    perl -pi -e 'sub _e {join(qq{,\n},
-            map(qq{  "$_": "$ENV{$_}"},
-                qw(SYNERGIA2DIR LD_LIBRARY_PATH PYTHONPATH)))};
-        s/^\{/{\n "env": {\n@{[_e()]}\n },/' "$where/kernel.json"
-}
-
-build_vars
+beamsim_jupyter_vars
