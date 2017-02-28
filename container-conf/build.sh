9e022a82a87925f7bd79a3222fc6e8bd2fdbd35a
robnagler <github@q33.us>
2017-02-28 15:20:19 +0000

    fpc: typo

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -70,8 +70,7 @@ build_as_root() {
     build_yum install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-21.noarch.rpm
     build_yum install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-21.noarch.rpm
     # ffmpeg for matplotlib animations
-    # yum-utils for yu
-m repo management
+    # yum-utils for yum repo management
     build_yum install ffmpeg yum-utils texlive-scheme-medium
     # ffmpeg was already installed from rpmfusion, disable it for future packages
     yum-config-manager --disable 'rpmfusion*' > /dev/null
