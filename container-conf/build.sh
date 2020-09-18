e7d573c6545e752b009597907ad2bb3b1a58b202
robnagler <github@q33.us>
2020-09-18 22:56:46 +0000

    fix #31 base is radiasoft/sirepo

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -113,7 +113,7 @@ beamsim_jupyter_rsbeams_style() {
 }
 
 beamsim_jupyter_vars() {
-    build_image_base=radiasoft/beamsim
+    build_image_base=radiasoft/sirepo
     beamsim_jupyter_boot_dir=$build_run_user_home/.radia-run
     beamsim_jupyter_radia_run_boot=$beamsim_jupyter_boot_dir/start
     build_is_public=1
