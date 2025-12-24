be1297bdda5bd9af2087c9a1a333a42934a344c0
Rob Nagler <5495179+robnagler@users.noreply.github.com>
2025-12-24 16:18:20 -0600

    Fix #116 Update to Fedora 43 & Python 3.13.9 (#119)
    - Remove fftw3-devel, pygmo needed by fbpic which was removed
    - Pegged all Python versions
    - Fix #117 added jupyterlab-h5web & mc
    - Fix #118 added numba

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -1,103 +1,104 @@
 #!/bin/bash
 
 beamsim_jupyter_base_jupyterlab() {
-    # https://github.com/jupyter/notebook/issues/2435
-    # installed by rpm-code/codes/rsbeams.sh, but here to
-    # document
-    install_assert_pip_version jedi 0.17.2 'check codes/rsbeams.sh'
     # POSIT: versions same in container-jupyterhub/build.sh
     declare x=(
-        ipympl==0.9.6
-        ipywidgets==7.6.5
-        jupyter==1.0.0
-        jupyter-packaging==0.10.6
-        git+https://github.com/radiasoft/jupyter-tensorboard-proxy.git
-        jupyterhub==1.4.2
-        jupyterlab-launcher==0.13.1
-        jupyterlab-server==2.8.2
-        jupyterlab==3.1.14
-        plotly
-        jupyterlab-favorites==3.0.0
-        jupyterlab-widgets==1.0.2
-        mlflow
+        # These lists were created by pip installing packages and seeing which versions were installed.
+        # The "Successfully installed" line which pip outputs
 
-        # modules users have requested
-        llvmlite
-        numba
+        # pip install jupyterlab
+        'jupyterlab==4.5.1'
+        'argon2-cffi==25.1.0'
+        'argon2-cffi-bindings==25.1.0'
+        'arrow==1.4.0'
+        'async-lru==2.0.5'
+        'beautifulsoup4==4.14.3'
+        'bleach==6.3.0'
+        'defusedxml==0.7.1'
+        'fastjsonschema==2.21.2'
+        'fqdn==1.5.1'
+        'isoduration==20.11.0'
+        'json5==0.12.1'
+        'jsonpointer==3.0.0'
+        'jupyter-events==0.12.0'
+        'jupyter-lsp==2.3.0'
+        'jupyter-server==2.17.0'
+        'jupyter-server-terminals==0.5.3'
+        'jupyterlab-pygments==0.3.0'
+        'jupyterlab-server==2.28.0'
+        'mistune==3.1.4'
+        'nbclient==0.10.3'
+        'nbconvert==7.16.6'
+        'nbformat==5.10.4'
+        'notebook-shim==0.2.4'
+        'pandocfilters==1.5.1'
+        'prometheus-client==0.23.1'
+        'python-json-logger==4.0.0'
+        'rfc3339-validator==0.1.4'
+        'rfc3986-validator==0.1.1'
+        'rfc3987-syntax==1.1.0'
+        'send2trash==1.8.3'
+        'soupsieve==2.8.1'
+        'terminado==0.18.1'
+        'tinycss2==1.4.0'
+        'uri-template==1.3.0'
+        'webcolors==25.10.0'
+        'webencodings==0.5.1'
+        'websocket-client==1.9.0'
 
-        # needs to be before fbpic https://github.com/radiasoft/devops/issues/153
-        pyfftw
-        # https://github.com/radiasoft/devops/issues/152
-        fbpic
+        # pip install ipympl
+        'ipympl==0.9.8'
+        'ipywidgets==8.1.8'
+        'jupyterlab_widgets==3.0.16'
+        'widgetsnbextension==4.0.15'
 
-# temporarily disable https://github.com/radiasoft/container-beamsim-jupyter-base/issues/40
-#        # https://github.com/radiasoft/jupyter.radiasoft.org/issues/75
-#        gpflow
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/10
-        GPy
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/11
-        safeopt
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/13
-        seaborn
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/39
-        botorch
-        # needed by zgoubidoo
-        parse
+        # pip install jupyter
+        'jupyter==1.1.1'
+        'jupyter-console==6.6.3'
+        'notebook==7.5.1'
 
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/32
-        # installs bokeh, too
-        git+https://github.com/slaclab/lume-genesis
-        eval-type-backport # lume-genesis missing dep
-        lark # lume-genesis missing dep
-        pydantic-settings # lume-genesis missing dep
-        git+https://github.com/ChristopherMayes/openPMD-beamphysics
-        git+https://github.com/radiasoft/zfel
+        # pip install jupyter-packaging
+        'jupyter-packaging==0.12.3'
+        'deprecation==2.1.0'
+        'tomlkit==0.13.3'
 
-        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/42
-        bluesky
+        # Individual packages (not depending on each other)
+        'jupyterlab-launcher==0.13.1'
+        'jupyterlab-favorites==3.3.1'
+        'plotly==6.5.0'
+
+        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/118
+        'numba==0.63.1'
+        'llvmlite==0.46.0'
+        'seaborn==0.13.2'
+
+        # jupyterhub
+        'jupyterhub==5.4.3'
+        'Mako==1.3.10'
+        'alembic==1.17.2'
+        'certipy==0.2.2'
+        'oauthlib==3.3.1'
+        'pamela==1.2.0'
+
+        # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/117
+        # pip install jupyterlab-h5web
+        'jupyterlab-h5web==12.6.1'
+        'h5grove==2.3.0'
+        'orjson==3.11.5'
     )
-    pip install "${x[@]}"
+    install_pip_install "${x[@]}"
     julia -e 'using Pkg; Pkg.add("IJulia")'
-
-    declare n
-    if install_version_fedora_lt_36; then
-        install_pip_install pygmo==2.16.1
-        n=
-    else
-        install_pip_install pygmo
-        n=--openssl-legacy-provider
-    fi
-    declare l=(
-        @jupyterlab/server-proxy
-        # Note: https://github.com/jupyterlab/jupyterlab/issues/5420
-        # will produce a collision (but warning) on vega-lite
-        jupyterlab-chart-editor@4.14.3
-    )
-    jupyter labextension install --no-build "${l[@]}"
-    # https://jupyterlab.readthedocs.io/en/stable/user/jupyterhub.html#use-jupyterlab-by-default
-    beamsim_jupyter_base_lab
-    # Need dev-build because jupyter lab build defaults to dev build
-    # when there are declare extensions (jupyter-rs-*)
-    # See https://git.radiasoft.org/radiasoft/container-beamsim-jupyter-base/issues/81 for reason behind NODE_OPTIONS
-    if ! NODE_OPTIONS="$n" jupyter lab build --dev-build=False; then
-        tail -100 /tmp/jupyterlab*.log || true
-        build_err 'juptyer lab failed to build'
-    fi
+    beamsim_jupyter_base_rs_widgets
 }
 
-beamsim_jupyter_base_lab() {
+beamsim_jupyter_base_rs_widgets() {
     declare f
-    declare p=$(pwd)
-    mkdir -p ~/src/radiasoft
     for f in jupyter_rs_vtk jupyter_rs_radia; do
-        cd ~/src/radiasoft
-        git clone https://github.com/radiasoft/"$f"
+        install_git_clone "$f"
         cd "$f"
-        pip install .
-        cd js
-        jupyter labextension install --no-build .
+        install_pip_install .
+        cd ..
     done
-    cd "$p"
 }
 
 beamsim_jupyter_base_rsbeams_style() {
@@ -106,12 +107,10 @@ beamsim_jupyter_base_rsbeams_style() {
     # https://github.com/radiasoft/container-beamsim-jupyter-base/issues/27
     declare d=~/.config/matplotlib/stylelib
     mkdir -p "$d"
-    git clone https://github.com/radiasoft/rsbeams
+    install_git_clone rsbeams
     for src in rsbeams/rsbeams/rsplot/stylelib/*; do
-        dst=$d/$(basename "$src")
-        cp "$src" "$dst"
+        cp "$src" "$d/$(basename "$src")"
     done
-    rm -rf rsbeams
 }
 
 beamsim_jupyter_base_vars() {
@@ -119,25 +118,27 @@ beamsim_jupyter_base_vars() {
     beamsim_jupyter_base_boot_dir=$build_run_user_home/.radia-run
     beamsim_jupyter_base_radia_run_boot=$beamsim_jupyter_base_boot_dir/start
     build_is_public=1
-    build_docker_cmd='["'"$beamsim_jupyter_base_radia_run_boot"'"]'
+    build_docker_cmd=$beamsim_jupyter_base_radia_run_boot
 }
 
 build_as_root() {
     umask 022
     declare r=(
         rscode-ipykernel
-        # https://github.com/radiasoft/devops/issues/188
+        # Needed to export notebooks https://github.com/radiasoft/devops/issues/188
         pandoc
-        # https://github.com/radiasoft/devops/issues/153
-        fftw3-devel
         vim-enhanced
         gnuplot-minimal
         ncl-devel
+
+        # Not installed in beamsim. See download/installers/beamsim-codes
         rscode-geant4
         rscode-julia
-        rscode-madness
-        rscode-genesis4
+
+        # USPAS (temporary?) https://github.com/radiasoft/container-beamsim-jupyter-base/issues/117
+        mc
     )
+
     build_yum install "${r[@]}"
     # Add RPMFusion repo for ffmpeg
     # http://rpmfusion.org/Configuration
@@ -146,7 +147,7 @@ build_as_root() {
     build_yum install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-"$e"
     build_yum install ffmpeg texlive-scheme-medium texlive-collection-latexextra
     # ffmpeg installed from rpmfusion so disable it for other packages
-    dnf config-manager --set-disabled 'rpmfusion*'
+    install_yum_repo_set_enabled 'rpmfusion*' 0
 }
 
 build_as_run_user() {
